495cf4b4204c85737e5742e72b4e58fb725b9883
SOVANORAK <norakeusever@gmail.com>
2024-10-24 10:57:01 +0700

    feat: nest-api

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
+import { EmployeesModule } from './employees/employees.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+
+@Module({
+  imports: [
+    UsersModule,
+    DatabaseModule,
+    EmployeesModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 5,
+      },
+    ]),
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
+})
+export class AppModule {}
---
+++ src/users/users.service.ts
@@ -0,0 +1,94 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { NotFoundException } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      email: 'Sincere@april.biz',
+      role: 'INTERN',
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      email: 'Shanna@melissa.tv',
+      role: 'INTERN',
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      email: 'Nathan@yesenia.net',
+      role: 'ENGINEER',
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      email: 'Julianne.OConner@kory.org',
+      role: 'ENGINEER',
+    },
+    {
+      id: 5,
+      name: 'Chelsey Dietrich',
+      email: 'Lucio_Hettinger@annie.ca',
+      role: 'ADMIN',
+    },
+  ];
+
+  //Find all
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) {
+      const roleArray = this.users.filter((user) => user.role === role);
+      if (roleArray.length === 0) throw new NotFoundException('Role Not Found');
+      return roleArray;
+    }
+    return this.users;
+  }
+
+  //FIND ONE
+  findOne(id: number) {
+    const user = this.users.find((user) => user.id === id);
+    if (!user) throw new NotFoundException('User Not Found');
+    return user;
+  }
+
+  //CREATE
+  create(createUserDto: CreateUserDto) {
+    const userByHighestId = [...this.users].sort((a, b) => b.id - a.id);
+
+    const newUser = {
+      id: userByHighestId[0].id + 1,
+      ...createUserDto,
+    };
+
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  //UPDATE
+  update(id: number, updateUserDto: UpdateUserDto) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return {
+          ...user,
+          ...updateUserDto,
+        };
+      }
+      return user;
+    });
+
+    return this.findOne(id);
+  }
+
+  //DELETE
+  delete(id: number) {
+    const removeUser = this.findOne(id);
+
+    this.users = this.users.filter((user) => user.id !== id);
+
+    return removeUser;
+  }
+}
